7830738e17945e5bcec5591ad5a5f82594e746da
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-08-20 21:54:29 -0300

    it now accept the path and extensions desired

--- my-shell-scripts/format_png_files.sh
+++ my-shell-scripts/format_png_files.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-number_of_files_to_change=$(ls *.png | wc -l)
-index=1
-current_date=$(date +'%Y-%m-%d')
+current_date=$(date +%F)
+workspace=$1
+extension=$2
+last_character="${1: -1}"
 
-while [ $index -le $number_of_files_to_change ]; do
-  file_to_rename=$(ls *.png | head -$index | tail -1)
-  mv "./$file_to_rename" "./$current_date-$file_to_rename"
-  index=$(expr $index + 1)
+cd $workspace
+
+for file in $(ls *.$extension); do
+  echo "Renomeando '$file' para '${current_date}-${file}'"
+  sleep 0.3
+  mv $file ${current_date}-${file}
 done
 
-## another - and better - solution
-# current_date=$(date +%F)
+## my first solution
 
-# for file in $(ls *.png); do
-#   mv $file ${current_date}-{$file}
+# number_of_files_to_change=$(ls *.png | wc -l)
+# index=1
+# current_date=$(date +'%Y-%m-%d')
+
+# while [ $index -le $number_of_files_to_change ]; do
+#   file_to_rename=$(ls *.png | head -$index | tail -1)
+#   mv "./$file_to_rename" "./$current_date-$file_to_rename"
+#   index=$(expr $index + 1)
 # done

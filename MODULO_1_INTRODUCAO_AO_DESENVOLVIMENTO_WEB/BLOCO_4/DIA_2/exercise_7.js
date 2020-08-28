cf2aa45f7c6fb95f0ebcf13cdc8e9d9c63fd12d9
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-08-28 13:30:25 -0300

    algoritmo melhorado, menos código

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_7.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_7.js
@@ -1,14 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorNumero = 0;
+let menorNumero = numbers[0];
 
-for (let index = 0; index < numbers.length; index++) {
-  if (numbers[index] > maiorNumero) maiorNumero = numbers[index];
-}
-
-let menorNumero = maiorNumero;
-
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 1; index < numbers.length; index++) {
   if (numbers[index] < menorNumero) menorNumero = numbers[index];
 }
 

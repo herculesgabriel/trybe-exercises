90bee820e5948c48422b2dfddb8fac4f71c982a5
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-08-28 12:55:48 -0300

    código que soma os valores de um array

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_2.js
@@ -0,0 +1,5 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+
+console.log(resultado);

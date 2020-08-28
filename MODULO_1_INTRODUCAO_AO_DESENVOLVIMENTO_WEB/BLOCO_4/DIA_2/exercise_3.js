27970221c4d938f09c25986698ad96b921143808
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-08-28 12:59:45 -0300

    código que retonrna a média aritmética dos itens de um array

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_3.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let quantidadeItems = numbers.length;
+let somaItems = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+let mediaAritmetica = somaItems / quantidadeItems;
+
+console.log(mediaAritmetica);

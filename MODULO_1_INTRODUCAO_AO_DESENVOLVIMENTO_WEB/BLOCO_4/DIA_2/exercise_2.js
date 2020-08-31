7752f81d8cc72415983e9e7dff83a689d0973a9d
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-08-31 15:45:24 -0300

    utilizando loop para otimizar soma

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_2.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_2/exercise_2.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
 
-let resultado = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+for (let index = 0; index < numbers.length; index += 1) {
+  resultado += numbers[index];
+}
 
 console.log(resultado);

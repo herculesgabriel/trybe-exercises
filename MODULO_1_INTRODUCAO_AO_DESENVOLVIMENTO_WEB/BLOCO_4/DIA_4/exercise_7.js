a54e8e4c845b68cb6d801e9e47ab9ff3b56c18cf
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-01 15:19:32 -0300

    consertado nome da função e removido console

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_4/exercise_7.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_4/exercise_7.js
@@ -1,15 +1,14 @@
 const valueTest = [2, 3, 6, 7, 10, 1];
 
-function whichIndexIsGreater(array) {
-  let greaterValue = array[0];
-  let greaterIndex = 0;  
+function whichIndexIsHigher(array) {
+  let higherValue = array[0];
+  let higherIndex = 0;
 
   for (let index in array) {
-    if (array[index] > greaterValue) {
-      console.log(`${array[index]} é maior que ${greaterValue}`);
-      greaterValue = array[index];
-      greaterIndex = index;
+    if (array[index] > higherValue) {
+      higherValue = array[index];
+      higherIndex = index;
     }
   }
-  return greaterIndex;
+  return higherIndex;
 }

b9ad284255010036cb4ac061ec27e748b1e1c930
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-01 15:30:57 -0300

    retorna o maior nome do array

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_4/exercise_9.js
@@ -0,0 +1,12 @@
+let valueTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function whichNameIsBiggest(arrayNames) {
+  let biggestName = arrayNames[0];
+
+  for (let index in arrayNames) {
+    if (arrayNames[index].length > biggestName.length) {
+      biggestName = arrayNames[index]
+    }
+  }
+  return biggestName;
+}

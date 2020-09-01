629704f840bda0202990718c9c7e49106f96029a
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-01 10:23:31 -0300

    scripts refatorados com funções

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_4/DIA_1/scripts/operacoes_matematicas.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function module(a, b) {
+  return a % b;
+}

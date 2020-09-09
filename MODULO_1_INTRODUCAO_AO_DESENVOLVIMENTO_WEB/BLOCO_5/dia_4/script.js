fc79238d93f936b8be1a1359a2b22ee586f0976a
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-09 12:06:04 -0300

    add change text color feature

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_5/dia_4/script.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_5/dia_4/script.js
@@ -1,12 +1,29 @@
 const body = document.getElementById('root');
 const bgColorSelect = document.getElementById('background-color');
+const textColorSelect = document.getElementById('text-color-select');
 
-bgColorSelect.addEventListener('change', () => {changeBackground(bgColorSelect.value)});
+bgColorSelect.addEventListener('change', () => { changeBackground(bgColorSelect.value) });
 
 function changeBackground(color) {
   body.style.backgroundColor = color;
-  
+
   if (color === 'black') body.style.color = 'white';
   if (color === 'white') body.style.color = 'black';
   if (color === 'LightYellow') body.style.color = 'black';
+}
+
+textColorSelect.addEventListener('change', () => { changeTextColor(textColorSelect.value) });
+
+function changeTextColor(color) {
+  let backgroundColor = body.style.backgroundColor;
+
+  if (color === 'auto') {
+    if (backgroundColor === 'black') {
+      body.style.color = 'white';
+    } else if (backgroundColor === 'white' || backgroundColor === 'lightyellow') {
+      body.style.color = 'black';
+    }
+  } else {
+    body.style.color = color;
+  }
 }

a223db1e3e7aa031ffd6f73d49028d1ec02829ac
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-17 19:51:45 -0300

    cria função que valida a data

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_6/dia_1/script.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_6/dia_1/script.js
@@ -1,4 +1,6 @@
-const statesList = document.getElementById('state');
+// const statesList = document.getElementById('state');
+const date = document.getElementById('date').value;
+const buttonSubmit = document.getElementById('button-submit');
 
 const states = [
   ['Acre', 'AC'],
@@ -30,13 +32,36 @@ const states = [
   ['Distrito Federal', 'DF'],
 ];
 
-function getStates() {
-  states.forEach((state) => {
-    const option = document.createElement('option');
-    option.setAttribute('value', state[1]);
-    option.textContent = state[0];
-    statesList.appendChild(option);
-  });
+// function getStates() {
+//   states.forEach((state) => {
+//     const option = document.createElement('option');
+//     option.setAttribute('value', state[1]);
+//     option.textContent = state[0];
+//     statesList.appendChild(option);
+//   });
+// }
+
+function validateDate() {
+  const day = date.substr(0, 2);
+  const month = date.substr(3, 2);
+  const year = date.substr(6, 4);
+
+  const validDay = (day > 0 && day <= 31);
+  const validMonth = (month > 0 && month <= 12);
+  const validYear = (year >= 0);
+
+  console.log(day)
+  console.log(month)
+  console.log(year)
+  console.log(validDay)
+  console.log(validMonth)
+  console.log(validYear)
+
+  return console.log(validDay && validMonth && validYear);
 }
 
-getStates();
+buttonSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  (validateDate()) ? console.log('ok') : console.log('mmm :(');
+});
+// getStates();

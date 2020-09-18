ed1d87866e409e6a82a7655a6eefc870285729dc
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-18 10:18:11 -0300

    dia concluído

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_6/dia_1/script.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_6/dia_1/script.js
@@ -1,6 +1,6 @@
-// const statesList = document.getElementById('state');
-const date = document.getElementById('date').value;
+const statesList = document.getElementById('state');
 const buttonSubmit = document.getElementById('button-submit');
+const result = document.getElementById('result');
 
 const states = [
   ['Acre', 'AC'],
@@ -32,16 +32,17 @@ const states = [
   ['Distrito Federal', 'DF'],
 ];
 
-// function getStates() {
-//   states.forEach((state) => {
-//     const option = document.createElement('option');
-//     option.setAttribute('value', state[1]);
-//     option.textContent = state[0];
-//     statesList.appendChild(option);
-//   });
-// }
+function getStates() {
+  states.forEach((state) => {
+    const option = document.createElement('option');
+    option.setAttribute('value', state[1]);
+    option.textContent = state[0];
+    statesList.appendChild(option);
+  });
+}
 
 function validateDate() {
+  const date = document.getElementById('date').value;
   const day = date.substr(0, 2);
   const month = date.substr(3, 2);
   const year = date.substr(6, 4);
@@ -50,18 +51,39 @@ function validateDate() {
   const validMonth = (month > 0 && month <= 12);
   const validYear = (year >= 0);
 
-  console.log(day)
-  console.log(month)
-  console.log(year)
-  console.log(validDay)
-  console.log(validMonth)
-  console.log(validYear)
+  return validDay && validMonth && validYear;
+}
 
-  return console.log(validDay && validMonth && validYear);
+function saveData() {
+  let dataList = {
+    Nome: document.getElementById('name').value,
+    Email: document.getElementById('email').value,
+    CPF: document.getElementById('cpf').value,
+    Endereço: document.getElementById('address').value,
+    Cidade: document.getElementById('city').value,
+    Estado: document.getElementById('state').value,
+    Resumo: document.getElementById('summary').value,
+    Cargo: document.getElementById('position').value,
+    Descrição: document.getElementById('description').value,
+  };
+  createAnswer(dataList);
+}
+
+function createAnswer(dataList) {
+  for (const key in dataList) {
+    const li = document.createElement('li');
+    li.innerHTML = `<strong>${key}</strong>: ${dataList[key]}`;
+    result.appendChild(li);
+  }
+}
+
+function returnError() {
+  result.innerHTML = `A data digitada é inválida.`
 }
 
 buttonSubmit.addEventListener('click', (event) => {
   event.preventDefault();
-  (validateDate()) ? console.log('ok') : console.log('mmm :(');
+  (validateDate()) ? saveData() : returnError();
 });
-// getStates();
+
+getStates();

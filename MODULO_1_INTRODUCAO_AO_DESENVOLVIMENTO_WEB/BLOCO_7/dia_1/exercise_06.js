b029e1522bfe0e17ac71ff85096e806883c21765
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-26 08:10:11 -0300

    ex 5 - array de habilidades

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_1/exercise_06.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_1/exercise_06.js
@@ -1,3 +1,11 @@
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Java',
+  'Linux'
+]
+
 const changeString = string => {
   const sentence = 'Tryber x aqui!';
   return sentence.replace('x', string);

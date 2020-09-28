8972a7f6dbfd94f9a3c26d4bcb90cebbf32e6739
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-28 18:50:10 -0300

    exercício 3 - parte 2

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
@@ -27,6 +27,10 @@ const addTime = (obj, key, value) => {
 console.log(addTime(lesson2, 'turno', 'manhã'));
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const getKeys = (obj) => Object.keys(obj);
+
+console.log(getKeys(lesson1))
+
 // Crie uma função para mostrar o tamanho de um objeto.
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 // Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:

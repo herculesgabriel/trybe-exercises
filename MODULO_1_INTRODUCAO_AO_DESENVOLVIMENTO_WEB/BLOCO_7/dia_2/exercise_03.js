f1987aa9e3bac592c87f8ab7d2b09a19524493b0
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-28 18:48:58 -0300

    exercício 3 - parte 1

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
@@ -0,0 +1,32 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+const addTime = (obj, key, value) => {
+  obj[key] = value;
+  return obj;
+};
+
+console.log(addTime(lesson2, 'turno', 'manhã'));
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie uma função para mostrar o tamanho de um objeto.
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:

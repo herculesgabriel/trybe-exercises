b8c9644271353756d0ae1940c08b2a91e3f83cb2
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-28 19:21:17 -0300

    exercício 3 - parte 7

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
@@ -52,6 +52,11 @@ const getTotalStudents = (objArr) => objArr.reduce((acc, { numeroEstudantes }) =
 console.log(getTotalStudents([lesson1, lesson2, lesson3]))
 
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matemática'
+const getValueByNumber = (obj, pos) => Object.values(obj)[pos]
+
+console.log(getValueByNumber(lesson1, 0));
 
 // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo
 

fd45a69b2ece4b3335f06855eaf8433a5664070f
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-28 19:02:18 -0300

    exercício 3 - parte 5

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
@@ -42,3 +42,6 @@ const getValues = (obj) => Object.values(obj)
 console.log(getValues(lesson1))
 
 // Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 })
+
+console.log(allLessons);

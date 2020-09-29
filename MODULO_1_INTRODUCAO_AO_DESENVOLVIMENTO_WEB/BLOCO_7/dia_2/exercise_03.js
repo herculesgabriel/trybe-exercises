137df3f0d1910db118009f62925e7ad62992af9b
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-28 21:37:12 -0300

    exercício 3 - parte 9

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_2/exercise_03.js
@@ -48,15 +48,15 @@ console.log(allLessons);
 
 // Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 const getTotalStudents = (objArr) => objArr.reduce((acc, { numeroEstudantes }) => acc + numeroEstudantes, 0)
-
+// TODO: usar objeto allLessons
 console.log(getTotalStudents([lesson1, lesson2, lesson3]))
 
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 // console.log(getValueByNumber(lesson1, 0));
 // Output: 'Matemática'
-const getValueByNumber = (obj, pos) => Object.values(obj)[pos]
+const getValueByNumber = (obj, pos) =>
 
-console.log(getValueByNumber(lesson1, 0));
+  console.log(getValueByNumber(lesson1, 0));
 
 // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
 // console.log(verifyPair(lesson3, 'turno', 'noite'));
@@ -71,5 +71,39 @@ console.log(verifyPair(lesson3, 'turno', 'noite'))
 console.log(verifyPair(lesson3, 'materia', 'Maria Clara'))
 
 // Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
+console.log('')
+const getStudentsWhoWatched = (obj) => {
+  let total = 0;
+  for (const key in obj)
+    if (obj[key]['materia'] === 'Matemática')
+      total += obj[key]['numeroEstudantes'];
+
+  return total;
+}
+
+console.log(getStudentsWhoWatched(allLessons))
+
+/*
+{
+  lesson1: {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã'
+  },
+  lesson2: {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+    turno: 'manhã'
+  },
+  lesson3: {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite'
+  }
+}
+*/
 
 // Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:

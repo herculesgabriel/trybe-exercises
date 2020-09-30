13e1b527d70cfd732ae2a9722d79f970ef8ccba3
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-30 11:00:37 -0300

    exercício 14

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_3/exercise_14.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+function getChange(payable, paid) {
+  if (payable > paid) throw new Error('paid value is not enough');
+
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  const change = [];
+  const { length } = coins;
+  let remaining = paid - payable;
+
+  // escreva seu código aqui...
+  for (let i = 0; remaining > 0; i += 1) {
+    for (const num of coins) {
+      if (num <= remaining) {
+        remaining -= num;
+        change.push(num);
+        break;
+      }
+    }
+  }
+  return change;
+}
+
+let result = getChange(1, 1); // no change/coins just an empty array
+let expected = [];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(215, 300); // expect an array containing [50, 20, 10, 5]
+expected = [50, 20, 10, 5];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(486, 600); // expect an array containing [100, 10, 2, 2]
+expected = [100, 10, 2, 2];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(12, 400); // expect an array containing [200, 100, 50, 20, 10, 5, 2, 1]
+expected = [200, 100, 50, 20, 10, 5, 2, 1];
+assert.deepStrictEqual(result, expected);
+
+assert.throws(() => { getChange(100, 10); }, /^Error: paid value is not enough$/);

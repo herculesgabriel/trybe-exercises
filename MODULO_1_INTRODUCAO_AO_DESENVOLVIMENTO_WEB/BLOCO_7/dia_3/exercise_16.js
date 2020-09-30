75a9e548946547ecdf2d49455d94be84cc124de1
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-30 11:35:42 -0300

    exercício 16

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_7/dia_3/exercise_16.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const removeMiddle = words => words.splice(Math.floor(words.length / 2), 1);
+
+const words = ['mouse', 'giraffe', 'queen', 'window', 'bottle'];
+const expectedWords = ['mouse', 'giraffe', 'window', 'bottle'];
+const expectedOutput = ['queen'];
+const output = removeMiddle(words);
+
+assert.deepStrictEqual(output, expectedOutput);
+assert.deepStrictEqual(words, expectedWords);

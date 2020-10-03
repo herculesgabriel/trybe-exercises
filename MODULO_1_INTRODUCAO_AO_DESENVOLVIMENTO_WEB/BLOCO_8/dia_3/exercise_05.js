78573e43c76dcccca182d9ec0b7340013b916802
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-03 15:20:11 -0300

    exercício 05

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_3/exercise_05.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_3/exercise_05.js
@@ -1,18 +1,21 @@
-    
+
 const assert = require('assert');
 
 const names = [
-  'Aanemarie',  'Adervandes',   'Akifusa',
-  'Abegildo',   'Adicellia',    'Aladonata',
-  'Abeladerco', 'Adieidy',  'Alarucha',
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
 
 
 function containsA() {
-  // escreva seu código aqui
+  return names.reduce((acc, name) => {
+    name.split('').map(char => {
+      if (char.toLowerCase() === 'a') acc += 1
+    })
+    return acc;
+  }, 0);
 }
 
-// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula
-
-assert.deepEqual(containsA(), 20);
+assert.deepStrictEqual(containsA(), 20);

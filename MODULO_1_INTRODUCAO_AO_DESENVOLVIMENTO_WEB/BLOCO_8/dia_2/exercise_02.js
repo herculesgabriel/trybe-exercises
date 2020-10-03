28ecfd5c66cba4f06f50ba410707dd2a4735cb6c
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-03 13:16:08 -0300

    exercício 04

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_2/exercise_02.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_2/exercise_02.js
@@ -100,6 +100,4 @@ function nameAndAge() {
   }).sort((a, b) => a.age - b.age);
 }
 
-// Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-
 assert.deepStrictEqual(nameAndAge(), expected_result);
--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_2/exercise_04.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_8/dia_2/exercise_04.js
@@ -65,51 +65,34 @@ const books = [
 
 const expected_result = [
   {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
 ]
 
-function fantasyOrScienceFiction() {
-  return books.filter(book => {
-    return book.genre === 'Ficção Científica' || book.genre === 'Fantasia'
-  });
+function oldBooks() {
+  const date = new Date();
+
+  return books.filter((book) => {
+    return date.getFullYear() - book.releaseYear > 60
+  }).sort((a, b) => a.releaseYear - b.releaseYear);
 }
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expected_result);
+assert.deepStrictEqual(oldBooks(), expected_result);

92ac25b76081a67e1b0929ba48e501c995254ae6
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-07 17:54:49 -0300

    exercício 01

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_10/dia_1/JEST/sum.test.js
@@ -0,0 +1,23 @@
+const sum = require('./sum');
+
+describe('sum two numbers', () => {
+
+  it('4 plus 5 equal to 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('0 plus 0 equal to 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('should throw a error with string values', () => {
+    expect(() => sum(4, '5')).toThrow();
+  });
+
+  it("the Error message should contain 'parameters must be numbers'", () => {
+    expect(() => {
+      sum(4, '5')
+    }).toThrow('parameters must be numbers');
+  });
+
+});

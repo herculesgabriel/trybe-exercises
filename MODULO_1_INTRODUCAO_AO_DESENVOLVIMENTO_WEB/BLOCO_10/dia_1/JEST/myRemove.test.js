94ca9275f7db794603c1386bbb348d7045c91525
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-07 18:23:10 -0300

    exercício 02

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_10/dia_1/JEST/myRemove.test.js
@@ -0,0 +1,27 @@
+const myRemove = require('./myRemove');
+
+describe('removes an specific item from the given array', () => {
+
+  it('remove 3 from [1, 2, 3, 4] should return [1, 2, 4]', () => {
+    const received = myRemove([1, 2, 3, 4], 3);
+    expect(received).toEqual([1, 2, 4]);
+  });
+
+  it('should not return [1, 2, 3, 4] when asked to remove the number 3', () => {
+    const received = myRemove([1, 2, 3, 4], 3);
+    expect(received).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('parameter is not changed inside array', () => {
+    const parameter = [1, 2, 3, 4];
+    const expected = parameter.slice();
+    myRemove(parameter, 3);
+    expect(expected).toEqual([1, 2, 3, 4]);
+  });
+
+  it('array did not change when the value does not exists inside array', () => {
+    const expected = myRemove([1, 2, 3, 4], 5);
+    expect(expected).toEqual([1, 2, 3, 4]);
+  });
+
+});

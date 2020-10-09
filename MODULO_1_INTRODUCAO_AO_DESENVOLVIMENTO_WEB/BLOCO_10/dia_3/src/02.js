db0f6a92e8291a77dceaa2a7a2939824226f4f1f
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-09 19:20:26 -0300

    exercício 04

---
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_10/dia_3/src/02.js
@@ -0,0 +1,7 @@
+const upperCase = string => string.toUpperCase();
+
+const firstLetter = string => string.charAt(0);
+
+const concatStrings = (str1, str2) => `${str1}${str2}`;
+
+module.exports = { upperCase, firstLetter, concatStrings };

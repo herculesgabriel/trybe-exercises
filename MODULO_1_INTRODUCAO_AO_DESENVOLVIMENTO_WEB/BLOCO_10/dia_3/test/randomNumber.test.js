58736c0810d63fb4412350115a5f7002150d1bf1
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-09 18:11:42 -0300

    exercício 02

--- MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_10/dia_3/test/randomNumber.test.js
+++ MODULO_1_INTRODUCAO_AO_DESENVOLVIMENTO_WEB/BLOCO_10/dia_3/test/randomNumber.test.js
@@ -18,4 +18,22 @@ describe('Generates a random number', () => {
 
   });
 
+  it('should slkfsjdklfjdklfjslk', () => {
+
+    file.randomNumber = jest
+      .fn()
+      .mockReturnValue(10);
+
+    file.randomNumber.mockImplementationOnce((a, b) => a / b);
+
+    expect(file.randomNumber(10, 2)).toBe(5);
+    expect(file.randomNumber).toHaveBeenCalled();
+    expect(file.randomNumber).toHaveBeenCalledTimes(1);
+    expect(file.randomNumber).toHaveBeenCalledWith(10, 2);
+
+    expect(file.randomNumber()).toBe(10);
+    expect(file.randomNumber).toHaveBeenCalledTimes(2);
+
+  });
+
 });

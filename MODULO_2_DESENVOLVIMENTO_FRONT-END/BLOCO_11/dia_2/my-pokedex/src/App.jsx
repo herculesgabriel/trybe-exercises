ba2640873431f0f61d1920218263c93ead0e17db
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-20 08:21:31 -0300

    exercício pokedex concluído

---
+++ MODULO_2_DESENVOLVIMENTO_FRONT-END/BLOCO_11/dia_2/my-pokedex/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pokedex from "./pages/Pokedex.jsx";
+import "./styles/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;

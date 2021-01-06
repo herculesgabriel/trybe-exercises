7d7da078a0d63553b071612df1650bc6c3f2e53f
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-06 11:40:58 -0300

    finished exercise 8

--- MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/routeSimpsons.js
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/routeSimpsons.js
@@ -1,12 +1,28 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  fs.readFile('simpsons.json', (err, data) => {
-    const file = data.toJSON();
-    res.status(200).send({ simpsons: file.data });
-  });
+const readSimpsonsFile = async () => {
+  const file = await fs.readFile('simpsons.json');
+  const stringData = file.toString('utf8');
+  const jsonData = JSON.parse(stringData);
+
+  return jsonData;
+};
+
+router.get('/', async (req, res) => {
+  const data = await readSimpsonsFile();
+
+  res.status(200).send({ data });
+});
+
+router.get('/:id', async (req, res) => {
+  const data = await readSimpsonsFile();
+
+  const { id } = req.params;
+  const character = data.find(char => char.id === id);
+
+  res.status(200).send({ personagem: character || [] });
 });
 
 module.exports = router;

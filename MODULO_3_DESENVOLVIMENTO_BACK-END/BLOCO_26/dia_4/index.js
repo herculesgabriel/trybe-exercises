591d0635d2de28d097631f0ff98559fdcf24b8d0
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-06 15:20:58 -0300

    finished bonus exercise 2

--- MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/index.js
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const routeSimpsons = require('./routeSimpsons');
+const auth = require('./auth');
+
 const app = express();
 
 app.use(express.json());
-app.use('/simpsons', routeSimpsons);
+
+app.use('/simpsons', auth, routeSimpsons);
 
 app.get('*', (req, res) => res.status(404).send('Not found'));
 
-app.use((err, req, res, next) => {
-  res.status(500).send(`Ops! Algo deu errado! ${err.message}`);
-});
+app.use((err, req, res, next) => res.status(500).send(`Erro: ${err.message}`));
 
 app.listen(3000, () => console.log('Server started at port 3000'));

a2346d46ba4bd77d43fcaaa27678433362783aef
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-06 10:13:59 -0300

    finished exercise 3

--- MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/server.js
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_4/server.js
@@ -3,8 +3,16 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/ping', (req, res) => {
   res.send({ message: 'Pong!' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  
+  res.send({ name: `Hello, ${name}` });
 });
 
 app.listen(port, () => console.log(`Server started at ${port} port`));

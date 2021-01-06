50a1c09d8ce701b5d27bbbcfd044e406cdfb0fb0
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-05 22:18:57 -0300

    finished exercise 4

--- MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_3/exercise3.js
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_26/dia_3/exercise3.js
@@ -1,24 +1,16 @@
 const fs = require('fs');
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const startTimeCount = Date.now();
 
-rl.question('Digite o caminho para o diretório: ', (path) => {
-  const startTimeCount = Date.now();
-  const strings = fs.readdirSync(path);
+const strings = fs.readdirSync('./');
 
-  const totalFileSize = strings.reduce((acc, file) => {
-    const data = fs.readFileSync(file);
-    return acc + data.byteLength;
-  }, 0);
+const totalFileSize = strings.reduce((acc, file) => {
+  const data = fs.readFileSync(file);
+  return acc + data.byteLength;
+}, 0);
 
-  const totalTime = Date.now() - startTimeCount;
-  console.log(`O diretório especificado contém ${strings.length} arquivos`);
-  console.log(`O tamanho total dos arquivos é de ${totalFileSize} bytes`);
-  console.log(`O tempo total de execução foi de ${totalTime} milissegundos`);
+const totalTime = Date.now() - startTimeCount;
 
-  rl.close();
-});
+console.log(`O diretório especificado contém ${strings.length} arquivos`);
+console.log(`O tamanho total dos arquivos é de ${totalFileSize} bytes`);
+console.log(`O tempo total de execução foi de ${totalTime} milissegundos`);

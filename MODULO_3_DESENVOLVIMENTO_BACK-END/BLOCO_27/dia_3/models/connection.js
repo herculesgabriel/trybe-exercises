e0c0ee64d906180f5b5e73ed426c41a32c134ad5
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-22 19:23:04 -0300

    update

--- MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_27/dia_3/models/connection.js
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_27/dia_3/models/connection.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const DB_NAME = process.env.DB_NAME;
-console.log(typeof DB_NAME);
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_USER = process.env.DB_USER;
 
 const connection = mysql.createPool({
-  host: 'localhost', // Se necessário, substitua pelo seu host, `localhost` é o comum
-  user: DB_USER, // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
-  password: DB_PASSWORD, // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
+  host: 'localhost',
+  user: DB_USER,
+  password: DB_PASSWORD,
   database: DB_NAME,
 });
 

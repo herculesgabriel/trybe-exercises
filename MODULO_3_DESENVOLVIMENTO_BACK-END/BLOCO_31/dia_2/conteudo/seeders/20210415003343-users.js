6d5f601dd1d97b0810920117d0cb4faa751b6db1
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-05-04 19:37:26 -0300

    update

---
+++ MODULO_3_DESENVOLVIMENTO_BACK-END/BLOCO_31/dia_2/conteudo/seeders/20210415003343-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          fullName: 'Leonardo',
+          email: 'leo@test.com',
+          createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+          updatedAt: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        {
+          fullName: 'JEduardo',
+          email: 'edu@test.com',
+          createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+          updatedAt: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};

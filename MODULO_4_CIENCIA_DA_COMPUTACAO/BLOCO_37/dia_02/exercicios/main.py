5927ff5272645bb42cc1a526c25a07b0156e8ee8
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-08-07 17:36:26 -0300

    exercício 1 finalizado

---
+++ MODULO_4_CIENCIA_DA_COMPUTACAO/BLOCO_37/dia_02/exercicios/main.py
@@ -0,0 +1,8 @@
+from exercise_01 import find_words
+
+word = ["atach", "welldonehoneyr"]
+words = [["cat", "bt", "hat", "tree", "caaat"], ["hello", "world", "students"]]
+
+print(f"Result for {word[0]} is: {find_words(words[0], word[0])}")
+print(f"Result for {word[1]} is: {find_words(words[1], word[1])}")
+print()
